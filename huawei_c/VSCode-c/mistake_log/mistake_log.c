cfc88fae9329d0ea7702442bf3843d26fc831cf3
gudxin <gdx1517253@163.com>
2020-02-19 19:21:54 +0800

    unfinished

--- huawei_c/VSCode-c/mistake_log/mistake_log.c
+++ huawei_c/VSCode-c/mistake_log/mistake_log.c
@@ -3,14 +3,14 @@
 
 int main()
 {
-    char str[1000],*router[2],*file_name[10],i=0,j=0,*p[1000];
+    char str[1000],*router[2],*file_name[10],i=0,j=0,k,l=0,*p[1000];
     int len;
     const char s1[2] = " ",s2[2] = "\\";
-    while(gets(str))
+    while(scanf("%s",str)!=EOF)
     {
+        i = 0;
         router[0] = strtok(str, s1);
-        router[1] = strtok(NULL, s1);
-        //printf("%s   %s\n", router[0], router[1]);
+        printf("%s      %s", router[0], strstr(str, s1));
         file_name[i] = strtok(router[0], s2);
         while(file_name[i] != NULL)
         {
@@ -21,14 +21,18 @@ int main()
         if(len <= 16)
         {
             p[j] = file_name[i];
-            printf("%s\n", p[j]);
+            //printf("%s\n", p[j]);
         }
         else
         {
             p[j] = file_name[i] + len - 16;
-            printf("%s\n", p[j]);
+            //printf("%s\n", p[j]);
         }
+        //strcat(p[j]," ");
+        //strcat(p[j], router[1]);
+       // printf("%s\n", p[j]);
         j++;
     }
+
     return 0;
 }
